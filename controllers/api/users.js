ffc70b51bc7fa2f046172036a605bbf096da99dd
guantanamobosch <luketyson17@gmail.com>
2023-06-23 16:27:47 -0600

    comments

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,10 +1,14 @@
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const User = require("../../models/user");
 
+// takes in POST request containing user sign up form data
 async function create(req, res) {
     try {
+        // stores a new item in the db
         const user = await User.create(req.body);
+
+        // creates a token with user data as payload
         const token = createJWT(user);
         res.json(token);
     } catch (error) {
@@ -12,7 +16,11 @@ async function create(req, res) {
     }
 }
 
+// takes in user data from the sign up form
 function createJWT(user) {
+    // creates a JSON Web Token storing user data as payload,
+    // signing the token with the SECRET in the .env, and
+    // setting expiration time as 24h from time of creation
     return jwt.sign({ user }, process.env.SECRET, { expiresIn: "24h" });
 }
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,5 +2,7 @@ const express = require("express");
 const router = express.Router();
 const usersCtrl = require("../../controllers/api/users");
 
-// create a user
+// "/api/users" route in server.js
+
+// create a user with users controller
 router.post("/", usersCtrl.create);
