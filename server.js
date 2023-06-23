2136c987448372af075cb49e6ea1c933671d42a7
guantanamobosch <luketyson17@gmail.com>
2023-06-23 10:48:44 -0600

    added middleware in server

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 // import modules
 const express = require("express"); // - https://expressjs.com/en/starter/installing.html
 const path = require("path"); // - https://www.npmjs.com/package/path
+const logger = require("morgan"); // - https://www.npmjs.com/package/morgan
 require("dotenv").config(); // - https://www.npmjs.com/package/dotenv#-documentation
 require("./config/database"); // - connect to MongoDB
 
 // storing invoked express object in variable 'app'
 const app = express();
 
+// middleware (called on all server requests)
+app.use(logger("dev"));
+app.use(express.json()); // https://expressjs.com/en/api.html
+app.use(express.static(path.join(__dirname, "build")));
+
 // setting the port
 const port = process.env.PORT || 3001;
+
+// api routes
 
 // express route handler (for all * routes)
 app.get("/*", function (req, res) {
