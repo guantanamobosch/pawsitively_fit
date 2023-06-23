421a3446e9e5ff14d56b16446c522b60da2a8f3f
guantanamobosch <luketyson17@gmail.com>
2023-06-23 09:14:22 -0600

    comments in config/database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,10 @@
+// local repo-level terminal cmd: 'npm i mongoose' & import into variable 'mongoose'
 const mongoose = require("mongoose");
 
+// mongoose .connect method accessing variable 'MONGO_DB_URI' in .env
 mongoose.connect(process.env.MONGO_DB_URI);
 
+// mongoose connection listener
 const db = mongoose.connection;
 
 db.on("connected", function () {
