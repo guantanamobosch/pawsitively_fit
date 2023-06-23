d42f231569543de6b3f602a64a71a7ddf3d97a49
wKategianes <wKategianes@gmail.com>
2023-06-23 16:06:22 -0700

    Mongoose Schemas

---
+++ models/pet.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Schema for pets
+const petSchema = new Schema ({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    photo: {
+        type: String
+    },
+    breed: {
+        type: String,
+        required: true
+    },
+    weight: {
+        type: Number,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    activity: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    medications: [{
+        type: String
+    }],
+    medicalhistory: [{
+        type: String
+    }],
+    vet: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Vet"       
+    }],
+},{
+    timestamps: true
+})
+
+const Pet = mongoose.model('Pet', petSchema);
+
+module.exports = Pet;
