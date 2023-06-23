dc810ec70845c51cef7e145c9c642f756f1418bc
alonzoalcantar <pinoa.03@outlook.com>
2023-06-23 12:42:46 -0700

    fixed file pathing for app component; header and dashboard display while running nodemon

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { HashRouter as Router } from 'react-router-dom'
-import App from './Pages/App/App'
+import App from './pages/App/App'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
