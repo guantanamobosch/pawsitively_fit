162c269b2e3888668aa70be47c2c7d7acb06cc14
guantanamobosch <luketyson17@gmail.com>
2023-06-24 12:48:51 -0600

    getBreedList function in dogs-api utility

--- src/pages/Resources/DogBreedGuide/DogBreedGuide.js
+++ src/pages/Resources/DogBreedGuide/DogBreedGuide.js
@@ -1,5 +1,7 @@
-import React from "react";
+import { React, useState } from "react";
 
 export default function DogBreedGuide() {
+    const [dogList, setDogList] = useState([]);
+
     return <div>DogBreedGuide</div>;
 }
--- src/utilities/dogs-utilities/dogs-api.js
+++ src/utilities/dogs-utilities/dogs-api.js
@@ -1,1 +1,21 @@
+import { json } from "react-router-dom";
 import { sendRequest } from "../send-request";
+const breedListUrl = "https://dog.ceo/api/breeds/list/all";
+
+export async function getBreedList() {
+    const parsedBreedList = await JSON.parse(sendRequest(breedListUrl));
+    const breedsObject = parsedBreedList.message;
+    const breedList = [];
+
+    Object.keys(breedsObject).forEach(function (key, index) {
+        const breedTypes = breedsObject[key].length;
+        if (breedTypes === 0) {
+            breedList.push(key);
+        } else {
+            for (let i = 0; i < breedTypes; i++) {
+                const joinedBreedName = breedsObject[key][i] + key;
+                breedList.push(joinedBreedName);
+            }
+        }
+    });
+}
