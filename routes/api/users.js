4332eed5ef126fa97bf1a947d7017c573b6724f6
eddie-hernandez <71785228+eddie-hernandez@users.noreply.github.com>
2023-06-24 18:00:11 -0400

    removing duplicate login function in users-service

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,12 +3,9 @@ const router = express.Router();
 const usersCtrl = require("../../controllers/api/users");
 
 // "/api/users" route from server.js
+router.get('/check-token', usersCtrl.checkToken);
 router.post("/", usersCtrl.create); // create a user
 router.post("/login", usersCtrl.login); // login
-
-// create a user with users controller
-router.get('/check-token', usersCtrl.checkToken);
-router.post("/", usersCtrl.create);
 
 
 module.exports = router;
