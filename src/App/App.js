b8f6753816f87753785876c6eb2f8b608bb6fc54
guantanamobosch <luketyson17@gmail.com>
2023-06-24 11:42:27 -0600

    normalizing file names

--- src/App/App.js
+++ src/App/App.js
@@ -1,39 +1,39 @@
-import './App.css'
-import { Routes, Route, Navigate} from 'react-router-dom'
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 // importing pages
-import Header from '../Components/Header/Header'
-import Footer from '../Components/Footer/Footer'
-import Navbar from '../Components/Navbar/Navbar'
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
+import Navbar from "../Components/Navbar/Navbar";
 
-import Appointments from '../pages/Appointments/Appointments'
-import Dashboard from '../pages/Dashboard/Dashboard'
-import Messages from '../pages/Messages/Messages'
-import Pets from '../pages/Pets/Pets'
-import Providers from '../pages/Providers/Providers'
-import Resources from '../pages/Resources/Resources'
-import Settings from '../pages/Settings/Settings'
+import Appointments from "../Pages/Appointments/Appointments";
+import Dashboard from "../Pages/Dashboard/Dashboard";
+import Messages from "../Pages/Messages/Messages";
+import Pets from "../Pages/Pets/Pets";
+import Providers from "../Pages/Providers/Providers";
+import Resources from "../Pages/Resources/Resources";
+import Settings from "../Pages/Settings/Settings";
 
 export default function App() {
-  return (
-    <main>
-      <Header />
-      <Navbar />
+    return (
+        <main>
+            <Header />
+            <Navbar />
 
-      <Routes>
-        {/* Establishing routes */}
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/appointments" element={<Appointments />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/pets" element={<Pets />} />
-        <Route path="/providers" element={<Providers />} />
-        <Route path="/resources" element={<Resources />} />
-        <Route path="/settings" element={<Settings />} />
-        {/* Routing catch all // homepage */}
-        <Route path="/*" element={<Navigate to="/dashboard" />} />
-      </Routes>
+            <Routes>
+                {/* Establishing routes */}
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/appointments" element={<Appointments />} />
+                <Route path="/messages" element={<Messages />} />
+                <Route path="/pets" element={<Pets />} />
+                <Route path="/providers" element={<Providers />} />
+                <Route path="/resources" element={<Resources />} />
+                <Route path="/settings" element={<Settings />} />
+                {/* Routing catch all // homepage */}
+                <Route path="/*" element={<Navigate to="/dashboard" />} />
+            </Routes>
 
-      <Footer />
-    </main>
-  )
+            <Footer />
+        </main>
+    );
 }
