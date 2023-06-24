5cc4b5c6e15c907b92a104ee1b5d69160031c19e
alonzoalcantar <pinoa.03@outlook.com>
2023-06-24 13:10:35 -0700

    Initial log in controller

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -4,7 +4,22 @@ const User = require("../../models/user");
 
 
 
+async function login(req, res) {
+    try {
 
+        const user = await User.findOne({email: req.body.email});
+        if (!user) throw new Error;
+
+        const match = await bcrypt.compare(req.body.password, user.password);
+        if (!match) throw new Error
+
+        const token = createJWT(user);
+        res.json(token);
+    } catch (err) {
+        res.status(400).json('Invalid Credentials')
+    }
+
+  }
 
 
 
