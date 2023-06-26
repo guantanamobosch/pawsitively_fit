c8d030c54c3f4d71ebf06b9ae7b052818873c861
guantanamobosch <luketyson17@gmail.com>
2023-06-25 18:02:43 -0600

    routes, paths, and elements in App.js for breed info and breed guide

--- src/pages/Resources/Resources.js
+++ src/pages/Resources/Resources.js
@@ -1,166 +1,173 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
 import "./Resources.css";
 
 // Material UI imports
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
+import DogBreedGuide from "./DogBreedGuide/DogBreedGuide";
 // import ResourceCard from '../../Components/ResourceCard/ResourceCard'
 // import SearchBar from '../../Components/SearchBar/SearchBar'
 
 export default function Resources() {
     return (
-        <div className="resources-div-container">
-            <div>
-                <h3 className="PageTitle">My Resources</h3>
-                {/* <SearchBar /> */}
+        <>
+            <div className="resources-div-container">
+                <div>
+                    <h3 className="PageTitle">My Resources</h3>
+                    {/* <SearchBar /> */}
+                </div>
+                <Grid container columnSpacing={2} rowSpacing={4}>
+                    <Grid
+                        item
+                        xs={2}
+                        sm={4}
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Card
+                            sx={{
+                                width: "100%",
+                                maxWidth: 500,
+                                maxHeight: 300,
+                                minHeight: 200,
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Typography variant="h4">
+                                Symptom Assessment
+                            </Typography>
+                        </Card>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={2}
+                        sm={4}
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Card
+                            sx={{
+                                width: "100%",
+                                maxWidth: 500,
+                                maxHeight: 300,
+                                minHeight: 200,
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Typography variant="h4">Breed Guide</Typography>
+                        </Card>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={2}
+                        sm={4}
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Card
+                            sx={{
+                                width: "100%",
+                                maxWidth: 500,
+                                maxHeight: 300,
+                                minHeight: 200,
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Typography variant="h4">Pet Training</Typography>
+                        </Card>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={2}
+                        sm={4}
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Card
+                            sx={{
+                                width: "100%",
+                                maxWidth: 500,
+                                maxHeight: 300,
+                                minHeight: 200,
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Typography variant="h4">Pet Travel</Typography>
+                        </Card>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={2}
+                        sm={4}
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Card
+                            sx={{
+                                width: "100%",
+                                maxWidth: 500,
+                                maxHeight: 300,
+                                minHeight: 200,
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Typography variant="h4">Pet Nutrition</Typography>
+                        </Card>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={2}
+                        sm={4}
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Card
+                            sx={{
+                                width: "100%",
+                                maxWidth: 500,
+                                maxHeight: 300,
+                                minHeight: 200,
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Typography variant="h4">Pet Shelters</Typography>
+                        </Card>
+                    </Grid>
+                </Grid>
             </div>
-            <Grid container columnSpacing={2} rowSpacing={4}>
-                <Grid
-                    item
-                    xs={2}
-                    sm={4}
-                    sx={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <Card
-                        sx={{
-                            width: "100%",
-                            maxWidth: 500,
-                            maxHeight: 300,
-                            minHeight: 200,
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    >
-                        <Typography variant="h4">Symptom Assessment</Typography>
-                    </Card>
-                </Grid>
-                <Grid
-                    item
-                    xs={2}
-                    sm={4}
-                    sx={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <Card
-                        sx={{
-                            width: "100%",
-                            maxWidth: 500,
-                            maxHeight: 300,
-                            minHeight: 200,
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    >
-                        <Typography variant="h4">Breed Guide</Typography>
-                    </Card>
-                </Grid>
-                <Grid
-                    item
-                    xs={2}
-                    sm={4}
-                    sx={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <Card
-                        sx={{
-                            width: "100%",
-                            maxWidth: 500,
-                            maxHeight: 300,
-                            minHeight: 200,
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    >
-                        <Typography variant="h4">Pet Training</Typography>
-                    </Card>
-                </Grid>
-                <Grid
-                    item
-                    xs={2}
-                    sm={4}
-                    sx={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <Card
-                        sx={{
-                            width: "100%",
-                            maxWidth: 500,
-                            maxHeight: 300,
-                            minHeight: 200,
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    >
-                        <Typography variant="h4">Pet Travel</Typography>
-                    </Card>
-                </Grid>
-                <Grid
-                    item
-                    xs={2}
-                    sm={4}
-                    sx={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <Card
-                        sx={{
-                            width: "100%",
-                            maxWidth: 500,
-                            maxHeight: 300,
-                            minHeight: 200,
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    >
-                        <Typography variant="h4">Pet Nutrition</Typography>
-                    </Card>
-                </Grid>
-                <Grid
-                    item
-                    xs={2}
-                    sm={4}
-                    sx={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <Card
-                        sx={{
-                            width: "100%",
-                            maxWidth: 500,
-                            maxHeight: 300,
-                            minHeight: 200,
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    >
-                        <Typography variant="h4">Pet Shelters</Typography>
-                    </Card>
-                </Grid>
-            </Grid>
-        </div>
+        </>
     );
 }
