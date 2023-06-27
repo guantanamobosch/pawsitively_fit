7187b8f80719dd518a8942c60e0c32872572fd8c
guantanamobosch <luketyson17@gmail.com>
2023-06-27 11:33:00 -0600

    dogBreedGuide map assigns alphabet ID to the dog at the start of each new letter's section

---
+++ src/pages/Resources/DogBreedGuide/BreedNameAlphabetScroller/BreedNameAlphabetScroller.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./BreedNameAlphabetScroller.css";
+
+export default function BreedNameAlphabetScroller() {
+    return (
+        <div className="Breed-Name-Alphabet-Scroller-Container">
+            BreedNameAlphabetScroller
+        </div>
+    );
+}
--- src/pages/Resources/DogBreedGuide/DogBreedGuide.js
+++ src/pages/Resources/DogBreedGuide/DogBreedGuide.js
@@ -1,9 +1,12 @@
 import { React, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { getBreedList } from "../../../utilities/dogs-utilities/dogs-api";
+import BreedNameAlphabetScroller from "./BreedNameAlphabetScroller/BreedNameAlphabetScroller";
 
 export default function DogBreedGuide() {
     const [dogList, setDogList] = useState([]);
+    const [selectedLetter, SetSelecterLetter] = useState("");
+    let dogLetter = "";
 
     async function getDogList() {
         console.log(
@@ -19,35 +22,70 @@ export default function DogBreedGuide() {
 
     return (
         <>
+            <BreedNameAlphabetScroller />
             {Array.isArray(dogList)
                 ? dogList.map((dog, index) => {
-                      if (dog.includes(" ")) {
-                          const nameArr = dog.split(" ");
-                          const firstNameLink =
-                              nameArr[1].charAt(0).toLowerCase() +
-                              nameArr[1].slice(1);
-                          const lastNameLink =
-                              nameArr[0].charAt(0).toLowerCase() +
-                              nameArr[0].slice(1);
-                          return (
-                              <Link
-                                  key={`${firstNameLink}-${lastNameLink}-${index}`}
-                                  to={`/resources/breed-guide/breed-info/${firstNameLink}-${lastNameLink}`}
-                              >
-                                  <p>{dog}</p>
-                              </Link>
-                          );
+                      if (index === 0 || dog[0] != dogLetter) {
+                          dogLetter = dog[0];
+                          if (dog.includes(" ")) {
+                              const nameArr = dog.split(" ");
+                              const firstNameLink =
+                                  nameArr[1].charAt(0).toLowerCase() +
+                                  nameArr[1].slice(1);
+                              const lastNameLink =
+                                  nameArr[0].charAt(0).toLowerCase() +
+                                  nameArr[0].slice(1);
+                              return (
+                                  <Link
+                                      key={`${firstNameLink}-${lastNameLink}-${index}`}
+                                      to={`/resources/breed-guide/breed-info/${firstNameLink}-${lastNameLink}`}
+                                      className="Breed-Guide-Link"
+                                  >
+                                      <p id={dog[0]}>{dog}</p>
+                                  </Link>
+                              );
+                          } else {
+                              const nameLink =
+                                  dog.charAt(0).toLowerCase() + dog.slice(1);
+                              return (
+                                  <Link
+                                      key={`${nameLink}-${index}`}
+                                      to={`/resources/breed-guide/breed-info/${nameLink}`}
+                                  >
+                                      <p id={dog[0]}>{dog}</p>
+                                  </Link>
+                              );
+                          }
                       } else {
-                          const nameLink =
-                              dog.charAt(0).toLowerCase() + dog.slice(1);
-                          return (
-                              <Link
-                                  key={`${nameLink}-${index}`}
-                                  to={`/resources/breed-guide/breed-info/${nameLink}`}
-                              >
-                                  <p>{dog}</p>
-                              </Link>
-                          );
+                          if (dog.includes(" ")) {
+                              const nameArr = dog.split(" ");
+                              const firstNameLink =
+                                  nameArr[1].charAt(0).toLowerCase() +
+                                  nameArr[1].slice(1);
+                              const lastNameLink =
+                                  nameArr[0].charAt(0).toLowerCase() +
+                                  nameArr[0].slice(1);
+                              return (
+                                  <Link
+                                      key={`${firstNameLink}-${lastNameLink}-${index}`}
+                                      to={`/resources/breed-guide/breed-info/${firstNameLink}-${lastNameLink}`}
+                                      className="Breed-Guide-Link"
+                                  >
+                                      <p>{dog}</p>
+                                  </Link>
+                              );
+                          } else {
+                              const nameLink =
+                                  dog.charAt(0).toLowerCase() + dog.slice(1);
+                              return (
+                                  <Link
+                                      key={`${nameLink}-${index}`}
+                                      to={`/resources/breed-guide/breed-info/${nameLink}`}
+                                  >
+                                      <p>{dog}</p>
+                                  </Link>
+                              );
+                          }
                       }
                   })
                 : null}
