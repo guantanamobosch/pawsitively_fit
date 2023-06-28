3febc8c5a0120d1cecfc93d32727c76a4e01c369
alonzoalcantar <pinoa.03@outlook.com>
2023-06-27 17:20:18 -0700

    Initial welcome page set up

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,8 +1,40 @@
-import React from 'react'
-import './Dashboard.css'
+import React, { useState, useEffect } from 'react';
+import './Dashboard.css';
+
 
 export default function Dashboard() {
+  const [loggedInUser, setLoggedInUser] = useState('');
+
+  useEffect(() => {
+    const fetchLoggedInUser = async () => {
+      try {
+  
+        const response = await fetch('');
+        if (response.ok) {
+          const user = await response.json();
+          setLoggedInUser(user.username);
+        } else {
+
+          console.error('Failed to fetch user data');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchLoggedInUser();
+  }, []);
+
+
+
+
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      {loggedInUser ? (
+        <h1 className="welcome-message">Welcome Back, {loggedInUser}</h1>
+      ) : (
+        <h1 className="welcome-message">Welcome to the Dashboard</h1>
+      )}
+    </div>
+  );
 }
