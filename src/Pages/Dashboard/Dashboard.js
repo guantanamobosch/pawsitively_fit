931a2ad4bde857c8ec2a4ba11413273c815a045a
alonzoalcantar <pinoa.03@outlook.com>
2023-06-27 17:57:53 -0700

    Dashboard cards set up

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,20 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './Dashboard.css';
 
-
 export default function Dashboard() {
   const [loggedInUser, setLoggedInUser] = useState('');
 
   useEffect(() => {
     const fetchLoggedInUser = async () => {
       try {
-  
         const response = await fetch('');
         if (response.ok) {
           const user = await response.json();
           setLoggedInUser(user.username);
         } else {
-
           console.error('Failed to fetch user data');
         }
       } catch (error) {
@@ -25,9 +22,6 @@ export default function Dashboard() {
     fetchLoggedInUser();
   }, []);
 
-
-
-
   return (
     <div>
       {loggedInUser ? (
@@ -35,6 +29,24 @@ export default function Dashboard() {
       ) : (
         <h1 className="welcome-message">Welcome to the Dashboard</h1>
       )}
+
+      <div className="card-container1">
+        <h1>Card 1</h1>
+        <p>This is the description for Card 1.</p>
+        <button>Button 1</button>
+      </div>
+
+      <div className="card-container2">
+        <h1>Card 2</h1>
+        <p>This is the description for Card 2.</p>
+        <button>Button 2</button>
+      </div>
+
+      <div className="card-container3">
+        <h1>Card 3</h1>
+        <p>This is the description for Card 3.</p>
+        <button>Button 3</button>
+      </div>
     </div>
   );
 }
